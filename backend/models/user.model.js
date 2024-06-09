90b26473bf1e4949050b5ea5e6278bb680b768d7
christopher odhiambo <caspianodhis@gmail.com>
2024-06-09 21:36:57 +0300

    auth controllers --all

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -53,3 +53,7 @@ const userSchema = new mongoose.Schema({
     }
 
 }, {timestamps: true})
+
+const User = mongoose.model("User", userSchema)
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import express from "express"
 import authRoutes from "./routes/auth.routes.js"
 import dotenv from "dotenv"
 import connectMongoDB from "./db/connectMongoDB.js"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.use(express.json()) // parse req.body
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
