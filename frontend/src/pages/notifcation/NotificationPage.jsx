2814fd9f593f1567eb4e11cb49dcf82e6fb2420d
christopher odhiambo <caspianodhis@gmail.com>
2024-06-15 21:53:28 +0300

    notifications query

--- frontend/src/pages/notifcation/NotificationPage.jsx
+++ frontend/src/pages/notifcation/NotificationPage.jsx
@@ -1,36 +1,51 @@
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
-
+import {useQuery, useMutation} from "@tanstack/react-query"
+import {toast} from "react-hot-toast"
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
+	const queryClient = useQuery()
+	const {data: notifications, isLoading} = useQuery({
+		queryKey: ["notifications"],
+		queryFn: async () => {
+			try {
+				const res = await fetch("/api/notifications")
+				const data = res.json()
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong")
+				}
+				return data
+			} catch (error) {
+				throw new Error(error)
+			}
+		}
+	})
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
+	const {mutate: deleteNotifications} = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch("/api/notifications", {
+					method: "DELETE"
+				})
+				const data = await res.json()
+
+				if(!res.ok) throw new Error(data.error || "Something went wrong")
+				return data;
+			} catch (error) {
+				throw new Error(error)
+			}
+		},
+		onSuccess: () => {
+			toast.success("Notifications deleted successfully")
+			queryClient.invalidateQueries({queryKey: ["notifications"]})
+		},
+		onError: (error) => {
+			toast.error(error.message)
+		}
+	})
 
 	return (
 		<>
