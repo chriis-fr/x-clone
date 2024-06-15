227b12489e7b643594e5938f0a6cfaeded5feaa8
christopher odhiambo <caspianodhis@gmail.com>
2024-06-15 23:20:08 +0300

    did not use hook hence 2 doubled codes

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -48,7 +48,7 @@ const ProfilePage = () => {
 		}
 	})
 
-	const {mutate:updateProfile, isPending:isUpdatingProfile} = useMutation({
+	const {mutateAsync:updateProfile, isPending:isUpdatingProfile} = useMutation({
 		mutationFn: async () => {
 			try {
 				const res = await fetch("/api/users/update", {
@@ -182,7 +182,11 @@ const ProfilePage = () => {
 								{(coverImg || profileImg) && (
 									<button
 										className='btn btn-primary rounded-full btn-sm text-white px-4 ml-2'
-										onClick={() => updateProfile()}
+										onClick={async() => {
+											await updateProfile({coverImg, profileImg})
+											setCoverImg(null)
+											setProfileImg(null)
+										}}
 									>
 										{isUpdatingProfile ? "Updating..." : "Save"}
 									</button>
